e381b600f21b8e138e270a9a32190749f2e7e3a5
Mac User <macuser@Mac-no-MacBook-Air-4.local>
2017-02-02 19:35:26 +0900

    fixed

--- script/common.js
+++ script/common.js
@@ -5,3 +5,10 @@ function reload(frameName){
 function display(id,mes){
     document.getElementById(id).innerHTML = mes
 }
+
+$(function () {
+  $(document).on('drop dragover', function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+  });
+});
