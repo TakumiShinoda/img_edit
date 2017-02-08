27ef53af861329e700e6b72adf35f6dea7fbd300
Mac User <macuser@Mac-no-MacBook-Air-4.local>
2017-02-09 02:42:56 +0900

    fix log

--- script/frame_main.js
+++ script/frame_main.js
@@ -1,22 +1,22 @@
 function onDrop(event) {
     var files = event.dataTransfer.files;
-    var info = "";
-    var nameList = [];
+    var dataList = [];
     var logList = [];
     
     for (var i = 0;i < files.length;i++){
 	var file_name = files[i].name;
-	
-	nameList[i] = [];
-	nameList[i][0] = file_name;
-	nameList[i][1] = files[i]
-	parent.frames.frame_log.add(file_name + ":Added");
-    }
 
-    console.log(nameList);
-    
+	if (files[i].type.indexOf("image") == 0){
+	    dataList[i] = [];
+	    dataList[i][0] = file_name;
+	    dataList[i][1] = files[i]
+	    parent.frames.frame_log.add(file_name + ":Added");   
+	}else{
+	    parent.frames.frame_log.add(file_name + ":not supported")
+	}
+    }
     parent.frames.frame_log.load();
-    parent.frames.frame_menu.addMenu(nameList);
+    parent.frames.frame_menu.addMenu(dataList);
     event.preventDefault();
 }
 
