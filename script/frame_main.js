3a689c055e55c26a88b180f241a0130c49c0ff20
Mac User <macuser@Mac-no-MacBook-Air-4.local>
2017-02-09 13:41:39 +0900

    fix bug and style

--- script/frame_main.js
+++ script/frame_main.js
@@ -2,19 +2,25 @@ function onDrop(event) {
     var files = event.dataTransfer.files;
     var dataList = [];
     var logList = [];
+    var j = 0;
     
     for (var i = 0;i < files.length;i++){
 	var file_name = files[i].name;
 
 	if (files[i].type.indexOf("image") == 0){
-	    dataList[i] = [];
-	    dataList[i][0] = file_name;
-	    dataList[i][1] = files[i]
+	    console.log("pass");
+	    dataList[j] = [];
+	    dataList[j][0] = file_name;
+	    dataList[j][1] = files[i]
+	    j++;
 	    parent.frames.frame_log.add(file_name + ":Added");   
 	}else{
 	    parent.frames.frame_log.add(file_name + ":not supported")
 	}
     }
+
+    console.log(dataList);
+    
     parent.frames.frame_log.load();
     parent.frames.frame_menu.addMenu(dataList);
     event.preventDefault();
